436536c3b5645ab7503ff2437e523820de235628
Janice Pepper <janice.pepper@accenture.com>
2024-07-02 15:09:56 -0600

    set up first get bookRouter

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const app = express();
 
 const port = process.env.port || 3000;
 
+const bookRouter = express.Router();
+bookRouter.route("/books").get((req, res) => {
+  const response = { hello: "This is my API" };
+  res.json(response);
+});
+
+app.use("/api", bookRouter);
+
 app.get("/", (req, res) => {
-  res.send("Welcome to my API!");
+  res.send("Welcome to my Nodemon API!");
 });
 
 app.listen(port, () => {
