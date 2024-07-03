3acf9fe0113640ff2a1af3b406ead70ca5b6d3ba
Janice Pepper <janice.pepper@accenture.com>
2024-07-03 14:16:31 -0600

    using query string from request

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ const port = process.env.port || 3000;
 const bookRouter = express.Router();
 bookRouter.route("/books").get((req, res) => {
   (async function getBooks() {
+    const { query } = req;
     try {
       await mongoose.connect(
         "mongodb+srv://janeaquafina:PSofAKVbDXNtqowd@books.84ky1vy.mongodb.net/bookdata?retryWrites=true&w=majority"
       );
       const Book = require("./models/bookModel");
-      const books = await Book.find();
+      const books = await Book.find(query);
       res.json(books);
     } catch (error) {
       debug(error.stack);
