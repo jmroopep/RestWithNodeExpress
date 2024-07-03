c129628c4c9cfc175048da10942a6abc53760c74
Janice Pepper <janice.pepper@accenture.com>
2024-07-02 18:35:12 -0600

    dont need to use db

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.port || 3000;
 const bookRouter = express.Router();
 bookRouter.route("/books").get((req, res) => {
   (async function getBooks() {
-    const db = await mongoose.connect(
+    await mongoose.connect(
       "mongodb+srv://janeaquafina:PSofAKVbDXNtqowd@books.84ky1vy.mongodb.net/bookdata?retryWrites=true&w=majority"
     );
     const Book = require("./models/bookModel");
