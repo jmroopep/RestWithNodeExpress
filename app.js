e139a4ccbbbb8841048b73bb4fa870e1cf9b202d
Janice Pepper <janice.pepper@accenture.com>
2024-07-03 11:10:02 -0600

    added debugging tools

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
+/* eslint-disable no-debugger */
 const express = require("express");
 const adminRouter = require("./routers/adminRouter.js");
 const mongoose = require("mongoose");
+const debug = require("debug")("app");
+const morgan = require("morgan");
 
 const app = express();
+app.use(morgan("combined"));
 
 const port = process.env.port || 3000;
 
 const bookRouter = express.Router();
 bookRouter.route("/books").get((req, res) => {
   (async function getBooks() {
-    await mongoose.connect(
-      "mongodb+srv://janeaquafina:PSofAKVbDXNtqowd@books.84ky1vy.mongodb.net/bookdata?retryWrites=true&w=majority"
-    );
-    const Book = require("./models/bookModel");
-    const books = await Book.find();
-    res.json(books);
+    try {
+      await mongoose.connect(
+        "mongodb+srv://janeaquafina:PSofAKVbDXNtqowd@books.84ky1vy.mongodb.net/bookdata?retryWrites=true&w=majority"
+      );
+      const Book = require("./models/bookModel");
+      const books = await Book.find();
+      res.json(books);
+    } catch (error) {
+      debug(error.stack);
+    }
   })();
 });
 
@@ -22,9 +30,10 @@ app.use("/api", bookRouter);
 app.use("/admin", adminRouter);
 
 app.get("/", (req, res) => {
+  debugger;
   res.send("Welcome to my Nodemon API!");
 });
 
 app.listen(port, () => {
-  console.log("Running on port " + port);
+  debug("Running on port " + port);
 });
