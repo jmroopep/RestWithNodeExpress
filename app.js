c8004b73864867fad0077dc9d8c8837679227cae
Janice Pepper <janice.pepper@accenture.com>
2024-07-02 18:32:14 -0600

    got mongoose reading books from db

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const adminRouter = require("./routers/adminRouter.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -7,9 +8,16 @@ const port = process.env.port || 3000;
 
 const bookRouter = express.Router();
 bookRouter.route("/books").get((req, res) => {
-  const response = { hello: "This is my API" };
-  res.json(response);
+  (async function getBooks() {
+    const db = await mongoose.connect(
+      "mongodb+srv://janeaquafina:PSofAKVbDXNtqowd@books.84ky1vy.mongodb.net/bookdata?retryWrites=true&w=majority"
+    );
+    const Book = require("./models/bookModel");
+    const books = await Book.find();
+    res.json(books);
+  })();
 });
+
 app.use("/api", bookRouter);
 app.use("/admin", adminRouter);
 
